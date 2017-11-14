42bc6b55bfa62aefd8e3697ee091aa6a1c7955a0
Kelsey Z <you@example.comzahandke@gmail.com>
2017-11-13 23:18:45 -0500

    fixed nav

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,20 +1,69 @@
-// Instantiate the Bootstrap carousel
-$('.multi-item-carousel').carousel({
-  interval: false
-});
+
+
+
+
+
+
+
+
+
+$( document ).ready(function() {
+
+      $('.multi-item-carousel').carousel({
+          interval: false
+      });
 
 // for every slide in carousel, copy the next slide's item in the slide.
 // Do the same for the next, next item.
-$('.multi-item-carousel .item').each(function(){
-  var next = $(this).next();
-  if (!next.length) {
-    next = $(this).siblings(':first');
-  }
-  next.children(':first-child').clone().appendTo($(this));
-  
-  if (next.next().length>0) {
-    next.next().children(':first-child').clone().appendTo($(this));
-  } else {
-  	$(this).siblings(':first').children(':first-child').clone().appendTo($(this));
-  }
+      $('.multi-item-carousel .item').each(function(){
+            var next = $(this).next();
+            if (!next.length) {
+              next = $(this).siblings(':first');
+          }
+
+          next.children(':first-child').clone().appendTo($(this));
+          
+          if (next.next().length>0) {
+              next.next().children(':first-child').clone().appendTo($(this));
+          } else {
+             $(this).siblings(':first').children(':first-child').clone().appendTo($(this));
+          }
+
+      });
+
+      TweenLite.from(".hero-text", 0.7, {left:100,autoAlph:0});
+      
+
+
+      var $img = $('.platypus-image');
+
+        TweenMax.set($img,{autoAlpha:0});
+
+        TweenMax.to($img,3, {autoAlpha:1});
+
+
+
+    
 });
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
